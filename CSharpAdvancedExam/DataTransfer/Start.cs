bb8a4956146bdceca3ec0764c5da66a55f118f0d
Emo <eiv.razgrad@gmail.com>
2018-10-26 11:31:18 +0300

    Add

--- CSharpAdvancedExam/DataTransfer/Start.cs
+++ CSharpAdvancedExam/DataTransfer/Start.cs
@@ -7,15 +7,20 @@ namespace DataTransfer
     {
         static void Main(string[] args)
         {
+            StartProject();
+        }
+
+        private static void StartProject()
+        {
             string newPattern = @"s\:([^\;]+)\;r\:([^\;]+)\;m--""([A-Za-z ]+)""";
-            
+
             int num = int.Parse(Console.ReadLine());
             int sum = 0;
 
             for (int i = 0; i < num; i++)
             {
                 string inputStr = Console.ReadLine();
-                MatchCollection match = Regex.Matches(inputStr, newPattern);   
+                MatchCollection match = Regex.Matches(inputStr, newPattern);
 
                 foreach (Match item in match)
                 {
@@ -25,13 +30,13 @@ namespace DataTransfer
                     senderP = GetData(item.Groups[1].Value);
                     recieverP = GetData(item.Groups[2].Value);
                     message = GetData(item.Groups[3].Value);
-                    string allDigitsFrom = item.Groups[1].Value.ToString()+item.Groups[2].Value.ToString();
+                    string allDigitsFrom = item.Groups[1].Value.ToString() + item.Groups[2].Value.ToString();
                     sum += IsDigit(allDigitsFrom);
                     Console.WriteLine($"{senderP} says \"{message}\" to {recieverP}");
                 }
             }
             int total = sum;
-            Console.WriteLine($"Total data transferred: {total}MB");   
+            Console.WriteLine($"Total data transferred: {total}MB");
         }
 
         private static int IsDigit(string value1)
